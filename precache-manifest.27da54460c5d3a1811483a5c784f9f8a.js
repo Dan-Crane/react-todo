d1230a9fc7b9808ddcd0c5bb39f1f7f724a15924
dani <gre4kaDISCOUNT@gmail.com>
2020-07-19 18:34:09 +0900

    Updates

---
+++ precache-manifest.27da54460c5d3a1811483a5c784f9f8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce79af34f68067cc0b5bf0ce721f065",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "aa4dd1cfca662e9ae2c1",
+    "url": "/react-todo/static/css/main.ee7108c9.chunk.css"
+  },
+  {
+    "revision": "d29eccf7e0509f8e1e88",
+    "url": "/react-todo/static/js/2.bd6124f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.bd6124f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa4dd1cfca662e9ae2c1",
+    "url": "/react-todo/static/js/main.7210907f.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  }
+]);
